6ce7f32dd70aeaaad1c56a4c79e2481e65882b7e
Justin Geronimo <justin.geronimo@baruchmail.cuny.edu>
2014-12-24 01:39:29 -0500

    Add specs and code for editing todo items

--- spec/controllers/todo_items_controller_spec.rb
+++ spec/controllers/todo_items_controller_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe TodoItemsController do
 
+end
+
+=begin        This was taken out because it is already in Capybara tests
   describe "GET 'index'" do
     it "returns http success" do
       get 'index'
       response.should be_success
-    end
-  end
-
-end
+=end
